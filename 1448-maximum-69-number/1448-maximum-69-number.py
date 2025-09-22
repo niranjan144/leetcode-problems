a45613d1ea33890efe5803702b39efb408b1b587
niranjan144 <niranjan222144@gmail.com>
2025-09-22 20:16:01 +0530

    Time: 0 ms (100%), Space: 17.7 MB (47.85%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximum69Number(self, num):
+        s = list(str(num))
+        for i in range(len(s)):
+            if s[i] == '6':
+                s[i] = '9'
+                break
+        return int("".join(s))
+        
