9ebc58cffeed603efb06ce2b89f0d94388d8a902
niranjan144 <niranjan222144@gmail.com>
2025-11-29 13:46:48 +0530

    Time: 55 ms (56.41%), Space: 51 MB (87.75%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        banned = set(bannedWords)
+        count = 0
+
+        for word in message:
+            if word in banned:
+                count += 1
+                if count == 2:   # We can stop early
+                    return True
+
+        return False
